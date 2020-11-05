2ad1ef6c0ad4c19dd911e6aeba4300344a07d06b
Dustin Rothschild <drothschild89@gmail.com>
2020-11-05 16:56:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 hash.each_with_object({}) do |(key, value), final_array|
   binding.pry
-value.each do |inner
+value.each do |inner_key, names|
 
  final_array
 end             
