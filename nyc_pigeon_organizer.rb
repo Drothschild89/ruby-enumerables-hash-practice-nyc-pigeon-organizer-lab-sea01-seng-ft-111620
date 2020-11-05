70b92a62e9ecc15d92d7d2f39bbce85437daf762
Dustin Rothschild <drothschild89@gmail.com>
2020-11-05 17:08:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,9 @@ hash.each_with_object({}) do |(key, value), final_array|
 value.each do |inner_key, names|
 
 names.each do |name|
-  if !final_array(name)
+  if !final_array[name]
+    final_array[name] = {}
+    
   binding.pry
 end
  final_array
